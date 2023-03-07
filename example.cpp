96df7c90ed2e409b18da71084e3864a2de0d2704
VikasMeshram2708 <meshramvikas744@gmail.com>
2023-03-07 22:08:28 +0530

    :tada: modified my example.cpp main template file

--- example.cpp
+++ example.cpp
@@ -1,12 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 const int N = 0;
 
+#define debug(x) cout << #x << " " << x << "\n";
+#define nline "\n"
+#define ll long long
+#define pb push_back
+#define po pop_back
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-
+	
 	
 	return 0;
 }
